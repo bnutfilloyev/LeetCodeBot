b37953c82746cab52648fd8024172cafa8937d3f
yoshlikmedia <yoshlikmedia@gmail.com>
2022-02-09 15:20:32 +0500

    Add Filters

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,12 @@
 from aiogram import Dispatcher
 
 from loader import dp
-# from .is_admin import AdminFilter
+from .admins import AdminFilter
+from .group_chat import IsGroup
+from .private_chat import IsPrivate
 
 
 if __name__ == "filters":
-    # dp.filters_factory.bind(AdminFilter)
-    pass
+    dp.filters_factory.bind(AdminFilter)
+    dp.filters_factory.bind(IsGroup)
+    dp.filters_factory.bind(IsPrivate)
