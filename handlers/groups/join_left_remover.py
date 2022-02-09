599d18dcb97b972f2827fe82b7fbc9670a359650
yoshlikmedia <yoshlikmedia@gmail.com>
2022-02-09 15:21:59 +0500

    add moderator

---
+++ handlers/groups/join_left_remover.py
@@ -0,0 +1,9 @@
+from aiogram import types
+
+from filters import IsGroup
+from loader import dp, bot
+
+
+@dp.message_handler(IsGroup(), content_types=[types.ContentType.NEW_CHAT_MEMBERS, types.ContentType.LEFT_CHAT_MEMBER])
+async def new_member(message: types.Message):
+    await message.delete()
