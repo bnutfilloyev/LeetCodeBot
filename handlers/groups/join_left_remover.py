cfbe98817c3d48c0714bb968c9d722f74c9b71e6
yoshlikmedia <yoshlikmedia@gmail.com>
2022-02-24 00:26:01 +0500

    bug fixed

--- handlers/groups/join_left_remover.py
+++ handlers/groups/join_left_remover.py
@@ -4,6 +4,7 @@ from filters import IsGroup
 from loader import dp
 
 
-@dp.message_handler(IsGroup(), content_types=[types.ContentType.NEW_CHAT_MEMBERS, types.ContentType.LEFT_CHAT_MEMBER])
+@dp.message_handler(IsGroup(), content_types=types.ContentType.NEW_CHAT_MEMBERS)
+@dp.message_handler(IsGroup(), content_types=types.ContentType.LEFT_CHAT_MEMBER)
 async def new_member(message: types.Message):
     await message.delete()
